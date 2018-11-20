41320cd4e4f84f50e884b504ea663098adfb9382
Jake <jcrawford19@gmail.com>
2018-11-19 19:20:10 -0500

    multiple examples of category calls

--- index.php
+++ index.php
@@ -50,6 +50,26 @@ get_header();
 						?>
 				</nav><!-- #categories-navigation -->
 				<?php
+					$categories = get_categories( array(
+						'orderby' => 'name',
+						'order'   => 'ASC',
+						'exclude' => array(1)
+					) );
+					
+					foreach( $categories as $category ) {
+						$category_link = sprintf( 
+							'<a href="%1$s" alt="%2$s">%3$s</a>',
+							esc_url( get_category_link( $category->term_id ) ),
+							esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ),
+							esc_html( $category->name )
+						);
+						
+						echo '<p>' . sprintf( esc_html__( 'Category: %s', 'textdomain' ), $category_link ) . '</p> ';
+						echo '<p>' . sprintf( esc_html__( 'Description: %s', 'textdomain' ), $category->description ) . '</p>';
+						echo '<p>' . sprintf( esc_html__( 'Post Count: %s', 'textdomain' ), $category->count ) . '</p>';
+					} 
+				?>
+				<?php
 					get_template_part( 'template-parts/content-category-menu', get_post_type() );
 				?>
 			</div>
--- template-parts/content-category-menu.php
+++ template-parts/content-category-menu.php
@@ -12,15 +12,33 @@
 	// Get variables.
 	global $post;
 	// Array of all categories of current post
-	$postCategoriesArray = wp_list_categories();
+	// List terms in a given taxonomy using wp_list_categories (also useful as a widget if using a PHP Code plugin)
+	
+	$orderby      = 'name'; 
+	$show_count   = false;
+	$pad_counts   = false;
+	$hierarchical = true;
+	$title        = '';
+	$exclude = array(1);
+	
+	$args = array(
+	'orderby'      => $orderby,
+	'show_count'   => $show_count,
+	'pad_counts'   => $pad_counts,
+	'hierarchical' => $hierarchical,
+	'title_li'     => $title,
+	'exclude' => $exclude
+	);
+
+	$postCategoriesArray = wp_list_categories( $args );
 	$postCategoriesSlugArray = [];
 	// Create array of Category Slugs
 	if(!empty($postCategoriesArray)){
 		foreach($postCategoriesArray as $slugCategory) {
-			array_push($postCategoriesSlugArray, $slugCategory->slug);
+			?>
+			<p><?php $slugCategory->slug ?></p>
+			<?php
 		}
 	}
-	// Category Slug array to string
-	$postCategories = implode(" ",$postCategoriesSlugArray);
 ?>
 <!-- #post-<?php the_ID(); ?> -->
